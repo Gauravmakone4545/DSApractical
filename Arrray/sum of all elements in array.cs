31cd73c30e33f97a3010167bc279ae1181523802
Gaurav makone <gauravmakone@gmail.com>
2025-12-01 15:30:17 +0530

    Sum off all elementes in array

---
+++ Arrray/sum of all elements in array.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, };
+        int sum=0;
+
+        foreach (int num in arr)
+        {
+            sum = sum + num;
+
+        }
+        Console.WriteLine(sum);
+    }
+}
