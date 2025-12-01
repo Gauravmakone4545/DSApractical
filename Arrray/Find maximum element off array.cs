11439a1b56cc03cf4af7f76ea1bf3b4e4e556f17
Gaurav makone <gauravmakone@gmail.com>
2025-12-01 19:07:13 +0530

    initial commit

---
+++ Arrray/Find maximum element off array.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5,99,100,10000,1221};
+        
+        int max = arr[0];
+
+        for(int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        //foreach (int num in arr)
+        //{
+
+        //    if (num > max)
+        //    {
+        //        max = num;
+        //    }
+        //}
+        Console.WriteLine($"max number is   {max}");
+    }
+}
+
