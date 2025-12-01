c1aa4cadb09d91eee5454e82545c17f259d6de7b
Gaurav makone <gauravmakone@gmail.com>
2025-12-01 14:50:11 +0530

    Add project files.

---
+++ Arrray/Find minimum from array/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 11, 21, 3, 22, 4 };
+        int min = arr[0];
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine($"min number is {min}");
+    }
+}
